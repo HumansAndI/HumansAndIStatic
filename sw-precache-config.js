01aedc4925490fb73a21dc409228b31786f65efb
Jeremy Cantu <jcantu521@gmail.com>
2017-02-28 17:11:15 -0600

    Stripped out sw-precache webpack plug-in, replaced with regular sw-precache and needed set-up

---
+++ sw-precache-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  staticFileGlobs: [
+    '**.html',
+    'app.bundle.js',
+    'styles/**.css',
+    'images/*',
+    'dist/assets/icons/*'
+  ],
+  navigateFallback: '/index.html'
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,3 @@
-var SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
-
 module.exports = {
     entry: "./src/main",
     output: { filename: "app.bundle.js" },
@@ -13,20 +11,5 @@ module.exports = {
     },
     resolve: {
         extensions: [".ts", ".js"]
-    },
-    plugins: [
-    new SWPrecacheWebpackPlugin(
-      {
-        cacheId: 'humans-and-i-sw',
-        filename: 'serviceWorker.js',
-        staticFileGlobs: [
-            'styles/**',
-            'images/**',
-        ],
-        mergeStaticsConfig: true, // if you don't set this to true, you won't see any webpack-emitted assets in your serviceworker config 
-        staticFileGlobsIgnorePatterns: [/\.map$/], // use this to ignore sourcemap files 
-        minify: true
-      }
-    ),
-  ]
+    }
 }
