33ac6391b36496aabe3873be7b48c9afc9140bf2
Jeremy Cantu <jcantu521@gmail.com>
2017-02-28 14:04:32 -0600

    Added service worker cache plugin to webpack build, need to test live

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,5 @@
+var SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
+
 module.exports = {
     entry: "./src/main",
     output: { filename: "app.bundle.js" },
@@ -11,5 +13,20 @@ module.exports = {
     },
     resolve: {
         extensions: [".ts", ".js"]
-    }
+    },
+    plugins: [
+    new SWPrecacheWebpackPlugin(
+      {
+        cacheId: 'humans-and-i-sw',
+        filename: 'serviceWorker.js',
+        staticFileGlobs: [
+            'styles/**',
+            'images/**',
+        ],
+        mergeStaticsConfig: true, // if you don't set this to true, you won't see any webpack-emitted assets in your serviceworker config 
+        staticFileGlobsIgnorePatterns: [/\.map$/], // use this to ignore sourcemap files 
+        minify: true
+      }
+    ),
+  ]
 }
